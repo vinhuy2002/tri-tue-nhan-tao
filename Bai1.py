f43fc318b0df17b67556bdf70730d6bd372f1ee2
Vĩnh Uy <vinhuy2002@gmail.com>
2022-09-08 21:32:37 +0700

    Bài tập 1

---
+++ Bai1.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+def createGraph():
+    canh=[
+        ["a", "b"], ["a", "g"], ["b", "c"], ["b", "g"],
+        ["g", "l"], ["g", "h"], ["c", "h"], ["h", "i"],
+        ["h", "m"], ["m", "i"], ["i", "d"], ["i", "e"],
+        ["i", "j"], ["i", "n"], ["d", "e"], ["e", "j"],
+        ["e", "f"], ["f", "k"], ["j", "k"], ["j", "n"]
+    ]
+    graph= defaultdict(list)
+    for i in canh:
+        a,b = i[0], i[1]
+        graph[a].append(b)
+        graph[b].append(a)
+    return graph
+
+def timDuong(graph, start, end):
+    visited = set()
+    visited.add(start)
+    return __timDuong(graph, start, end, visited)
+
+def __timDuong(graph, start, end, visited):
+    if start == end:
+        return [start]
+    
+    if start in graph:
+        for i in graph[start]:
+            if i not in visited:
+                visited.add(i)
+                duongDi = __timDuong(graph, i, end, visited)
+                if duongDi is not None:
+                    duongDi.insert(0, start)
+                    return duongDi
+    
+    return "Khong co duong di!"
+
+if __name__ == "__main__":
+    graph = createGraph()
+    print(timDuong(graph, 'a', 'k'))
